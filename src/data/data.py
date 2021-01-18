3cc5b18f4e9ce4fdd4d58a68f4f5ea3a86a311be
MatthijsMars <matthijs.mars97@gmail.com>
2021-01-18 15:37:59 +0000

    feat: added data loading functionality to src/data/data.py
    - added function to load metadata
    - added functions to find transcript and audio paths from information in metadata
    - added function for loading and (partially) preprocessing transcript data

---
+++ src/data/data.py
@@ -0,0 +1,71 @@
+__all__ = [ "load_metadata", "find_file_paths", "load_transcript", 
+            "retrieve_full_transcript", "load_audio" ]
+
+DATA_PATH = "/unix/cdtdisspotify/data/spotify-podcasts-2020/"
+
+import json
+import os
+import numpy as np
+import pandas as pd 
+
+
+def load_metadata():
+    return pd.read_csv(DATA_PATH + "metadata.tsv", delimiter='\t')
+
+def find_file_paths(show_filename_prefix, episode_filename_prefix):
+    """
+    input:
+        - show_filename_prefix -> as given in metadata-*.tsv
+        - episode_filename_prefix -> as given in metadata-*.tsv
+     
+    returns:
+        - path to transcript .json file
+        - path to audio .ogg file
+        
+    # TODO also include the sets for summarization-testset
+    """
+    relative_file_path = os.path.join(
+        show_filename_prefix[5],
+        show_filename_prefix[6],
+        show_filename_prefix,
+        episode_filename_prefix
+    )
+    
+    transcript_path = os.path.join(
+        DATA_PATH,
+        "podcasts-transcripts/",
+        relative_file_path + ".json"   
+    )
+    
+    audio_path = os.path.join(
+        DATA_PATH,
+        "podcasts-transcripts/",
+        relative_file_path + ".json"   
+    )
+    
+    return transcript_path, audio_path
+
+
+def load_transcript(path):
+    """
+    returns a python dictionary with the .json transcript
+    """
+    with open(path, "r") as file:
+        transcript = json.load(file)
+    return transcript
+
+
+def retrieve_full_transcript(transcript_json):
+    """
+    returns the full transcript without timestamps or speakertags
+    """
+    
+    transcript = ""
+    for result in transcript_json["results"][:-1]:
+        transcript += result["alternatives"][0]['transcript']
+    return transcript
+
+
+def load_audio(path):
+    # TODO implement this feature
+    raise NotImplementedError
