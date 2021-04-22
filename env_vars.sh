0fadd7865a738c68b68d3ddcc262497bf04dbc18
jtingey <j.tingey.16@ucl.ac.uk>
2021-04-22 16:52:20 +0100

    updated env_vars script to remove laughter-detection path

--- env_vars.sh
+++ env_vars.sh
@@ -9,7 +9,6 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VGGISH_PATH="${DIR}/deps/tf-models/research/audioset/vggish/"
 YAMNET_PATH="${DIR}/deps/tf-models/research/audioset/yamnet/"
-LAUGHTER_PATH="${DIR}/deps/laughter-detection/"
 
 # Check the directories exist
 if [[ ! -d $VGGISH_PATH ]]; then
@@ -20,14 +19,9 @@ if [[ ! -d $YAMNET_PATH ]]; then
     echo "Can't find YAMNET_PATH!"
 fi
 
-if [[ ! -d $LAUGHTER_PATH ]]; then
-    echo "Can't find LAUGHTER_PATH!"
-fi
-
 # update environment variables
 export PODCAST_PATH=$DIR
 export VGGISH_PATH=$VGGISH_PATH
 export YAMNET_PATH=$YAMNET_PATH
-export LAUGHTER_PATH=$LAUGHTER_PATH
-export PYTHONPATH=$PYTHONPATH:$VGGISH_PATH:$YAMNET_PATH:$LAUGHTER_PATH
+export PYTHONPATH=$PYTHONPATH:$VGGISH_PATH:$YAMNET_PATH
 export TOKENIZERS_PARALLELISM="false"
