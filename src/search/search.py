cf40627f564f13ab6fdd25dc97ee85f162204817
jtingey <j.tingey.16@ucl.ac.uk>
2021-04-22 16:55:17 +0100

    feat: updated search procedure

--- src/search/search.py
+++ src/search/search.py
@@ -3,26 +3,23 @@
 """Script utilities."""
 
 import os
+import time
 import requests
 import argparse
 import pickle
 
+from tqdm import tqdm
 from omegaconf import OmegaConf
 import numpy as np
 import torch
 from transformers import (
     AutoModelForSequenceClassification,
     AutoTokenizer,
-    T5ForConditionalGeneration,
-    T5Tokenizer,
 )
-from tensorflow.keras.models import load_model
 import soundfile as sf
 import opensmile
-import resampy
-import laugh_segmenter  # noqa: E402
-import params as yamnet_params  # noqa: E402
-import yamnet as yamnet_model  # noqa: E402
+import params as yamnet_params
+import yamnet as yamnet_model
 
 from src.data import load_metadata, find_paths
 import src.utils
@@ -53,14 +50,6 @@ class Searcher:
         self.rerank_model.to("cpu", non_blocking=True)
         self.rerank_max_seq_len = 512
 
-        # Set up the emotion model
-        self.emotion_tokenizer = T5Tokenizer.from_pretrained(
-            conf.search_emotion_model, cache_dir=conf.search_cache_dir
-        )
-        self.emotion_model = T5ForConditionalGeneration.from_pretrained(
-            conf.search_emotion_model, cache_dir=conf.search_cache_dir
-        )
-
         # Set up the openSMILE extractor
         self.smile = opensmile.Smile(
             feature_set=opensmile.FeatureSet.eGeMAPSv02,
@@ -85,12 +74,7 @@ class Searcher:
             os.path.join(os.getenv("PODCAST_PATH"), "data/yamnet.h5")
         )
 
-        # Set ip the LaughterDetection model
-        self.laughter_model = load_model(
-            os.path.join(os.getenv("LAUGHTER_PATH"), "models/model.h5")
-        )
-
-    def search(self, topic_query, topic_desc=None, mood="entertaining", num=10):
+    def search(self, topic_query, topic_desc=None):
         """Run the full search and reranking pipeline for a query."""
         # 1) Run the basic Elasticsearch query on the segments index
         segments, es_scores = self.elasticsearch_query(topic_query, topic_desc)
@@ -98,30 +82,30 @@ class Searcher:
         # 2) Get the BERT(MS MARCO) rerank scores for the segments
         rerank_scores = self.get_rerank_scores(segments, topic_query, topic_desc)
 
-        # 3) Get the T5(GoEmotions) emotion scores for the segments
-        emotion_scores = self.get_emotion_scores(segments)
-
-        # 4) Run a rerank now, and only compute audio features on interesting segments
+        # 3) Run a rerank now, and only compute audio features on interesting segments
         segments_slim = [seg for i, seg in enumerate(segments) if rerank_scores[i] > 0.0]
-        es_scores_slim = [score for i, score in enumerate(es_scores) if rerank_scores[i] > 0.0]
-        rerank_scores_slim = [score for score in rerank_scores if score>0.0]
-        emotion_scores_slim = [score for i, score in enumerate(emotion_scores) if rerank_scores[i] > 0.0]
+        es_scores_slim = np.array([score for i, score in enumerate(es_scores) if rerank_scores[i] > 0.0])
+        rerank_scores_slim = np.array([score for score in rerank_scores if score>0.0])
         
-        # 5) Get the audio features for the segments
-        audio_features = self.get_audio_features(segments_slim)
+        # 4) Get the audio features for the segments
+        opensmile_scores, yamnet_scores = self.get_audio_scores(segments_slim)
 
-        # 6) Combine all the scores
+        # Think you want to take the top n reranked episodes and then work on those
+
+        return segments_slim, es_scores_slim, rerank_scores_slim, opensmile_scores, yamnet_scores 
+
+    def rerank(self, segments, es_scores, rerank_scores, opensmile_scores, yamnet_scores, mood="entertaining", num=10):
         if mood == "entertaining":
             ranked_segments = self.rerank_entertaining(
-                segments_slim, rerank_scores_slim, emotion_scores_slim, audio_features
+                segments, es_scores, rerank_scores, opensmile_scores, yamnet_scores
             )
         elif mood == "subjective":
             ranked_segments = self.rerank_subjective(
-                segments_slim, rerank_scores_slim, emotion_scores_slim, audio_features
+                segments, es_scores, rerank_scores, opensmile_scores, yamnet_scores
             )
         elif mood == "discussion":
             ranked_segments = self.rerank_discussion(
-                segments_slim, rerank_scores_slim, emotion_scores_slim, audio_features
+                segments, es_scores, rerank_scores, opensmile_scores, yamnet_scores
             )
         else:
             raise ValueError("Not a valid mood query!")
@@ -129,15 +113,24 @@ class Searcher:
         return ranked_segments
 
     def elasticsearch_query(self, topic_query, topic_desc=None):
-        """Run a topical search query on the Elasticsearch index."""
-        print("Running Elasticsearch query...")
-        # TODO: Look at a more complex query using the "topic_desc"
+        """Run a topical search query on the Elasticsearch index.
+        
+        Use the "topic_query" and "topic_desc" to search the 
+        "seg_words", "epis_name", "epis_desc" fields. We boost the 
+        "seg_words" by a factor of 2 as it is much more important.
+        """
+        print("Running Elasticsearch query... ", end = '')
+        start_time = time.time()
+        if topic_desc:
+            topic_input = topic_query + " " + topic_desc
+        else:
+            topic_input = topic_query
         json = {
             "size": self.es_num,
             "query": {
                 "multi_match": {
-                    "query": topic_query,
-                    "fields": ["seg_words", "epis_name", "epis_desc"],
+                    "query": topic_input,
+                    "fields": ["seg_words^2", "epis_name", "epis_desc"],
                 }
             },
         }
@@ -154,6 +147,7 @@ class Searcher:
             seg_dict["seg_id"] = seg["_id"]
             segments.append(seg_dict)
         scores = np.array([seg["_score"] for seg in response])
+        print("returned {} segments in {:.2f} seconds".format(len(scores), time.time() - start_time))
         return segments, scores
 
     def elasticsearch_test(self, num_segments=10):
@@ -166,8 +160,12 @@ class Searcher:
         return save_dict["segments"][:num_segments], save_dict["scores"][:num_segments]
 
     def get_rerank_scores(self, segments, topic_query, topic_desc=None):
-        """Get the rerank model scores for the given segments."""
-        print("Getting rerank scores for segments...")
+        """Get the rerank model scores for the given segments.
+        
+        Use the "query_title" and "query_desc" vs the "seg_words", "epis_name", and "epis_desc" fields.
+        """
+        print("Getting rerank scores for segments... ", end = '')
+        start_time = time.time()
         # Create the topic query and description input
         if topic_desc:
             topic_input = topic_query + " " + topic_desc
@@ -175,9 +173,7 @@ class Searcher:
             topic_desc = topic_query
 
         # Create the choices to rank against, we use "seg_words" and "epis_desc" fields
-        choices = [
-            choice["seg_words"] + " " + choice["epis_desc"] for choice in segments
-        ]
+        choices = [seg["seg_words"] + " " + seg["epis_name"] + " " + seg["epis_desc"] for seg in segments]
 
         # Tokenise the topic_input and choice pairs
         inputs = [
@@ -218,27 +214,11 @@ class Searcher:
                 input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids
             )[0]
             logits = logits.detach().cpu().numpy()
+        scores = np.array([score[1] for score in logits])
 
         # We just return the second number in the logits, may want to check this
-        return np.array([score[1] for score in logits])
-
-    def get_emotion_scores(self, segments):
-        """Get the emotion scores for the given segments."""
-        print("Getting emotion scores for segments...")
-        # Run each segment through the emotion model
-        emotions = []
-        for seg in segments:
-            input_text = "emotion: {}".format(seg["seg_words"])
-            features = self.emotion_tokenizer([input_text], return_tensors="pt")
-            tokens = self.emotion_model.generate(
-                input_ids=features["input_ids"],
-                attention_mask=features["attention_mask"],
-                max_length=64,
-            )
-            emotions.append(
-                self.emotion_tokenizer.decode(tokens[0], skip_special_tokens=True)
-            )
-        return np.array(emotions)
+        print("returned {} scores in {:.2f} seconds".format(len(scores), time.time() - start_time))
+        return scores
 
     def get_paths_and_starts(self, segments):
         """Get the audio file paths for the given segments."""
@@ -267,36 +247,18 @@ class Searcher:
         waveform = np.mean(waveform, axis=1) / 32768.0
         return waveform
 
-    def get_laughter_scores(self, waveform, threshold=0.5, min_length=0.2):
-        # Resample waveform to sr=8000
-        y_low = resampy.resample(waveform, self.sample_rate, 8000)
-
-        # Create the features and run through laughter model
-        feature_list = laugh_segmenter.get_feature_list(y_low, 8000)
-        probs = self.laughter_model.predict(feature_list)
-        probs = probs.reshape((len(probs),))
-        filtered = laugh_segmenter.lowpass(probs)
-        instances = laugh_segmenter.get_laughter_instances(
-            filtered, threshold=threshold, min_length=min_length
-        )
-
-        if len(instances) > 0:
-            return [{"start": i[0], "end": i[1]} for i in instances]
-        else:
-            return []
-
     def get_audio_scores(self, segments):
         """Get the audio features for the given segments.
 
-        Here we get the LaughterDetection, YAMNet, and openSMILE features for the segments.
+        Here we get the YAMNet and openSMILE features for the segments.
         """
-        print("Getting audio scores for segments...")
+        print("Getting audio scores for {} segments... ".format(len(segments)))
         # Get the audio paths for the segments
         paths, starts = self.get_paths_and_starts(segments)
 
         # Run each segment through each of the audio models and feature extractors
-        smile_score_list, yamnet_scores_list, laughter_scores_list = [], [], []
-        for path, start in zip(paths, starts):
+        smile_score_list, yamnet_scores_list = [], []
+        for path, start in zip(tqdm(paths), starts):
             # Get the segment audio
             waveform = self.get_audio_segment(path, start)
 
@@ -308,57 +270,69 @@ class Searcher:
             yamnet_scores, embeddings, spectrogram = self.yamnet_model(waveform)
             yamnet_scores_list.append(yamnet_scores.numpy())
 
-            # Run through the LaughterDetection model
-            laughter_scores = self.get_laughter_scores(waveform)
-            laughter_scores_list.append(laughter_scores)
+        return smile_score_list, yamnet_scores_list
 
-        return smile_score_list, yamnet_scores_list, laughter_scores_list
+    def rerank_entertaining(self, segments, rerank_scores, opensmile_scores, yamnet_scores):
+        """Rerank the topical segments according to the "entertaining" mood.
+        
+        The segment is topically relevant to the topic description AND the topic is 
+        presented in a way which the speakers intend to be amusing and entertaining to 
+        the listener, rather than informative or evaluative.
+        """
+        # Accept podcasts with a a low music frequency score
+        music_cut = np.array([src.search.yamnet_freq_feature(seg, 132) for seg in yamnet_scores]) < 100
 
-    def rerank_entertaining(
-        self, segments, rerank_scores, emotion_scores, audio_features
-    ):
-        # TODO: Probably want to rerank in bands as Jussi suggested
-        # TODO: Can do lots of rejection on things we don't want, music, quality, num speakers
-        # TODO: Need to remember that LaughterDetection gets confused with music
+        # Accept podcasts with a high laughter frequence score
+        laughter_freq_cut = np.array([src.search.yamnet_freq_feature(seg, 13) for seg in yamnet_scores]) > 5
+
+        # Accept podcasts with a high YAMNet funny score
+        laughter_feature_cut = np.array([src.search.yamnet_is_funny(seg)[0][1] for seg in yamnet_scores]) > 0.18
+
+        # Combine cuts and return 
+
         # rerank_ranks = np.argsort(rerank_scores_filter)[::-1]
         raise NotImplementedError
 
-    def rerank_subjective(
-        self, segments, rerank_scores, emotion_scores, audio_features
-    ):
-        # TODO: Probably want to rerank in bands as Jussi suggested
-        # TODO: Can do lots of rejection on things we don't want, music, quality, num speakers
-        # TODO: Need to remember that LaughterDetection gets confused with music
+    def rerank_subjective(self, segments, rerank_scores, opensmile_scores, yamnet_scores):
+        """Rerank the topical segments according to the "subjective" mood.
+        
+        The segment is topically relevant to the topic description AND the speaker or 
+        speakers explicitly and clearly express a polar opinion about the query topic, 
+        so that the approval or disapproval of the speaker is evident in the segment.
+        """
+        # Remove podcasts with a high music score
+        music_cut = np.array([src.search.yamnet_freq_feature(seg, 132) for seg in yamnet_scores]) > 100
+
+        # May want to get rid of laughter segments
+        
+        # Can use the two openSMILE scores to see if people are "serious" and then use
+        # speakers, conversation, and narration scores to seperate!
+
+        # 1) Get the "is_disapproval" feature for each of the segments
+        # 2) Get the "is_debate" feature for each of the segments
         raise NotImplementedError
 
-    def rerank_discussion(
-        self, segments, rerank_scores, emotion_scores, audio_features
-    ):
-        # TODO: Probably want to rerank in bands as Jussi suggested
-        # TODO: Can do lots of rejection on things we don't want, music, quality, num speakers
-        # TODO: Need to remember that LaughterDetection gets confused with music
+    def rerank_discussion(self, segments, rerank_scores, opensmile_scores, yamnet_scores):
+        """Rerank the topical segments according to the "discussion" mood.
+        
+        The segment is topically relevant to the topic description AND includes more 
+        than one speaker participating with non-trivial topical contribution (e.g. mere grunts, 
+        expressions of agreement, or discourse management cues ("go on", "right", "well, 
+        I don't know ..." etc) are not sufficient).
+        """
+        # 1) Get the "is_discussion" feature for each of the segments
+        # 2) Get the number of segment speakers based on the transcript diarisation
+        # 3) Get the "is_debate" feature for each of the segments
+        # 4) Get the "is_conversation" feature for each of the segments
         raise NotImplementedError
-
-
-def print_segments(segments):
-    """Pretty-print the search response segments."""
-    for seg in segments:
-        print(
-            "Segment ID: {}, Words: {}, Show Name: {}, Episode Name: {}".format(
-                seg["seg_id"],
-                seg["seg_length"],
-                seg["show_name"],
-                seg["epis_name"],
-            )
-        )
-        print("\n{}\n\n".format(seg["seg_words"]))
 
 
 def parse_args():
     """Parse the command line arguments."""
     parser = argparse.ArgumentParser(description="Performs a topical segment search")
-    parser.add_argument("query", help="search query")
-    parser.add_argument("--desc", help="search description", default=None)
+    parser.add_argument("query", help="topical query title")
+    parser.add_argument("--desc", help="topical query description", default=None)
+    parser.add_argument("--mood", help="mood query category", default=None)
     parser.add_argument("-n", "--num", help="Number of results to return", default=10)
     return parser.parse_args()
 
@@ -369,16 +343,9 @@ def main():
     if not args:
         raise ValueError("Invalid Arguments")
 
-    # Setup the GPUs
-    src.utils.gpu_setup()
-
-    # Setup the searcher
-    searcher = Searcher()
-
-    # Run the query
+    src.utils.gpu_setup()  # Setup the GPUs
+    searcher = Searcher()  # Setup the searcher
     results = searcher.search(args.query, args.desc, args.num)
-
-    # Print the results
     print(results)
 
 
